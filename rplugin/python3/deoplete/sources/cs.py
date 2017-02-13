03b95979af368067509f8b36ab89e6ae69c0058f
Jeff Li <junfengl@ca.ibm.com>
2017-02-13 15:18:34 -0500

    Remove unused import.

--- rplugin/python3/deoplete/sources/cs.py
+++ rplugin/python3/deoplete/sources/cs.py
@@ -4,7 +4,7 @@ import urllib
 import urllib.request
 import urllib.parse
 from .base import Base
-from deoplete.util import get_simple_buffer_config, error
+
 
 class Source(Base):
     def __init__(self, vim):
@@ -77,5 +77,3 @@ class Source(Base):
                 dup=1))
 
         return completions
-
-
